9d1042ce0102a7ecd1f32f7848137e9d2b70926b
ggueno <ggueno@ggueno-linux.(none)>
2013-01-11 00:24:17 +0100

    add Profile models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    """Model for countries"""
+    iso_code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=45, blank=False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+        ordering = ["name", "iso_code"]
+
+
+class Address(models.Model):
+    """Model to store addresses for accounts"""
+    address_line1 = models.CharField("Address line 1", max_length=45)
+    address_line2 = models.CharField("Address line 2", max_length=45,
+        blank=True)
+    postal_code = models.CharField("Postal Code", max_length=10)
+    city = models.CharField(max_length=50, blank=False)
+    country = models.ForeignKey(Country, blank=False)
+
+    def __unicode__(self):
+        return "%s, %s" % (self.city,
+                              str(self.country))
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+        unique_together = ("address_line1", "address_line2", "postal_code",
+                           "city", "country")
+
+
+class SocialNetwork(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    url = models.URLField(blank=False)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    about = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    #django enum : TODO
+    #status = models
+    address = models.ManyToManyField(Address)
+    social_network = models.ManyToManyField(SocialNetwork)
+
+
+class School(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+
+
+class Applicant(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100, blank=False)
+    search_location = models.CharField(max_length=100)
+    social_network = models.ManyToManyField(SocialNetwork)
+
+
+class Experience(models.Model):
+    company = models.OneToOneField(Company)
+    city = models.CharField(max_length=100)
+    start = models.DateField(blank=False, null=False)
+    end = models.DateField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
--- projecteel/settings.py
+++ projecteel/settings.py
@@ -1,4 +1,6 @@
 # Django settings for projecteel project.
+import os.path
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sql',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -25,11 +27,10 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
+TIME_ZONE = 'Europe/Paris'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 SITE_ID = 1
 
@@ -46,12 +47,14 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/ggueno/django/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://localhost/media/'
+
+ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -96,6 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    './templates',
 )
 
 ROOT_URLCONF = 'projecteel.urls'
@@ -117,9 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'core',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
