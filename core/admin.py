7a3b984f22657663c92818dd0a20e61cf95bf512
ggueno <ggueno@ggueno-linux.(none)>
2013-01-14 16:13:12 +0100

    add project views

---
+++ core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from core.models import *
+
+admin.site.register(Country)
+admin.site.register(Address)
+admin.site.register(SocialNetwork)
+admin.site.register(Company)
+admin.site.register(School)
+admin.site.register(Experience)
+admin.site.register(Applicant)
+admin.site.register(Education)
+admin.site.register(Skills)
+admin.site.register(CategoryOffer)
+admin.site.register(Offer)
+admin.site.register(Equipment)
+admin.site.register(Media)
+admin.site.register(Project)
