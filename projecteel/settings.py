1be394df2243e7f49aca8e93e2069f325005bbc6
ggueno <ggueno@ggueno-linux.(none)>
2013-01-15 11:32:42 +0100

    Add Projecteel Template and static files

--- projecteel/settings.py
+++ projecteel/settings.py
@@ -47,29 +47,42 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-
-
 #  : creer un fichier user_settings.py avec les variables en commentaires
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media/')
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = s.path.join(PROJECT_PATH, 'static/')
 
-# # Absolute filesystem path to the directory that will hold user-uploaded files.
-# # Example: "/var/www/example.com/media/"
-# MEDIA_ROOT = '/home/ggueno/django/media'
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
 
-# # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# # trailing slash.
-# # Examples: "http://example.com/media/", "http://media.example.com/"
-# MEDIA_URL = 'http://localhost/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# ADMIN_MEDIA_PREFIX = '/admin_media/'
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates/')
+)
+
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/ggueno/django/django_projects/projecteel/media/static",
+    os.path.join(PROJECT_PATH, 'media/static'),
 )
 
 # List of finder classes that know how to find static files in
