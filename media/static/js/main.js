07818142a2d6ecbf3ba54a146ce07780ac2278b3
durrentom <durrentom@gmail.com>
2013-01-21 16:32:13 +0100

    New grid system + CSS changes

--- media/static/js/main.js
+++ media/static/js/main.js
@@ -2,3 +2,16 @@
 var django = {
     "jQuery": jQuery.noConflict(true)
 };
+$ = django.jQuery;
+var options = {
+	inputs: {
+	  // The name attribute of the input in quotes
+	  'PTinput': {
+	    filters: 'required min',
+	    data: { min: 10 },
+	    errors: { min: 'At least 10 characters' },
+	    flags: 'noclass noinvalidicon'
+	  }
+	}
+}
+// var $myform = $('#projectFilters').idealforms(options).data('idealforms');
