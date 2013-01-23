44deb29c4563f55c5271e641e96179b300af614e
durrentom <durrentom@gmail.com>
2013-01-23 12:13:33 +0100

    Added new font Open Sans + Windows hack test

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,30 @@
-// django jQuery namespace
-var django = {
-    "jQuery": jQuery.noConflict(true)
-};
-$ = django.jQuery;
 var options = {
 	inputs: {
-	  // The name attribute of the input in quotes
-	  'PTinput': {
-	    filters: 'required min',
-	    data: { min: 10 },
-	    errors: { min: 'At least 10 characters' },
-	    flags: 'noclass noinvalidicon'
-	  }
-	}
+	    'username': {
+	      filters: 'required username exclude',
+	      data: { exclude: ['user', 'username', 'admin'] }
+	    },
+	    'date': { filters: 'date' },
+	    'comments': {
+	      filters: 'min max',
+	      data: { min: 50, max: 200 }
+	    },
+	    'colors': {
+	      filters: 'exclude',
+	      data: { exclude: ['default'] },
+	      errors: { exclude: 'Choisissez une couleur de la liste' }
+	    },
+	    'langs[]': {
+	      filters: 'min',
+	      data: { min: 2 },
+	      errors: { min: 'Check at least <strong>2</strong> languages.' }
+	    },
+	    'options': {
+	      filters: 'min',
+	      data: { min: 1 },
+	      errors: { min: 'Check only <strong>1</strong> option.' }
+	    }
+	},
+	responsiveAt: 100
 }
 var $myform = $('#my-form').idealforms(options).data('idealforms');
